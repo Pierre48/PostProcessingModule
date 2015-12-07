a9ccc0f17c5e3706ac851aa35700b35bb2d3015c
Pierre Plagnes <pierre.plagnes@gmail.com>
2015-12-07 23:48:11 +0100

    externalize configuration to an xml file

---
+++ Sources/PostProcessingModule/PostRequest/Config.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostProcessing.PostRequest
+{
+    internal class Configuration
+    {
+
+
+        static Configuration _config;
+
+
+        internal static Configuration Current
+        {
+            get
+            {
+                if (_config == null) throw new Exception("Not initialized");
+                return _config;
+            }
+        }
+
+        public List<Rule> Rules { get; private set; }
+
+        private static void IntializeFromWwwRoot()
+        {
+            var request = HttpWebRequest.Create(@"http://localhost/PostProcessingConfiguration.xml");
+            request.Headers.Add("Cache-Control", "private, max-age=0, no-cache");
+            var response = request.GetResponse();
+
+            var serializer = new DataContractSerializer(typeof(List<Rule>));
+            _config.Rules = serializer.ReadObject(response.GetResponseStream()) as List<Rule>;
+        }
+        internal static void Intialize()
+        {
+            _config = new Configuration();
+            IntializeFromWwwRoot();
+        }
+    }
+}
--- Sources/PostProcessingModule/PostRequest/LegacyHandler.cs
+++ Sources/PostProcessingModule/PostRequest/LegacyHandler.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using PostProcessing.PostRequest;
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Json;
+using System.Text;
 using System.Web;
 
 namespace PostProcessing
@@ -8,74 +12,7 @@ namespace PostProcessing
     /// The handler that will allow to change http response
     /// </summary>
     public class LegacyHandler : IHttpModule
-    {
-        /// <summary>
-        /// The _rules
-        /// </summary>
-        private static List<Rule> _rules = new List<Rule>
-        {
-            new Rule
-            {
-                ContentTypes = new List<string> {"application/javascript","jk" },
-                Changes = new List<Change>
-                {
-                    new Change
-                    {
-                        OldString = "The response is 42",
-                        NewString = "The response is 42(That is a js request)",
-                    }
-                }
-            },
-            new Rule
-            {
-                FilePathEndWith = new List<string> {"sample.js","jk" },
-                Changes = new List<Change>
-                {
-                    new Change
-                    {
-                        OldString = "The response is 42",
-                        NewString = "The response is 42(inserted text)",
-                    }
-                }
-            },
-            new Rule
-            {
-                FilePathEndWith = new List<string> {"WebResource.axd" },
-                Changes = new List<Change>
-                {
-                    new Change
-                    {
-                        OldString = "if(ig_shared.IsIE&&ig_shared.IsWin)",
-                        NewString = "if(false)",
-                    }
-                }
-            },
-            new Rule
-            {
-                FilePathEndWith = new List<string> {"WebResource.axd" },
-                Changes = new List<Change>
-                {
-                    new Change
-                    {
-                        OldString = "if (typeof (obj[item]) != \"undefined\" && obj[item] != null && !obj[item].tagName && !obj[item].disposing && typeof (obj[item]) != \"string\")",
-                        NewString = "if (typeof (obj[item]) != \"undefined\" && obj[item] != null && !obj[item].tagName && !obj[item].disposing && typeof (obj[item]) != \"string\" && obj.hasOwnProperty(item))",
-                    }
-                }
-            },
-            new Rule
-            {
-                FilePathEndWith = new List<string> {"ig_webgrid.js" },
-                Changes = new List<Change>
-                {
-                    new Change
-                    {
-                        OldString = "if (typeof (obj[item]) != \"undefined\" && obj[item] != null && !obj[item].tagName && !obj[item].disposing && typeof (obj[item]) != \"string\")",
-                        NewString = "if (typeof (obj[item]) != \"undefined\" && obj[item] != null && !obj[item].tagName && !obj[item].disposing && typeof (obj[item]) != \"string\" && obj.hasOwnProperty(item))",
-                    }
-                }
-            }
-        };
-
+    { 
         private HttpApplication _context;
 
         private HttpRequest Request
@@ -94,6 +31,7 @@ namespace PostProcessing
 
         public void Init(HttpApplication context)
         {
+
             _context = context;
 
             _context.PreRequestHandlerExecute += _context_PreRequestHandlerExecute;
@@ -101,9 +39,10 @@ namespace PostProcessing
 
         private void _context_PreRequestHandlerExecute(object sender, EventArgs e)
         {
+            Configuration.Intialize();
             var application = (HttpApplication) sender;
 
-            foreach (var rule in _rules)
+            foreach (var rule in Configuration.Current.Rules)
             {
                 if (rule.Check(Request, Response))
                 {
